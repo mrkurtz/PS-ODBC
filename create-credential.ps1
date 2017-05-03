4890148df426ef5fa25c1b03ff292fa01db53984
mrkurtz <mrkurtz@192.168.1.254>
2017-05-03 00:59:36 -0500

    removing old general files

--- create-credential.ps1
+++
@@ -1,7 +0,0 @@
-function create-credential ($passwordFile,$userName)
-    {
-        $password=get-content $passwordFile | convertto-securestring
-        $username=$userName
-        $credential=new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
-        return $credential
-    }
